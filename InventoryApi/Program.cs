e0f6ec0a660ba6feb7ec4b1abf247c11b60894a2
Mladen Shkrbikj <skrbicc555@gmail.com>
2025-05-22 14:36:39 +0200

    Added hardcoded passwords for database

--- InventoryApi/Program.cs
+++ InventoryApi/Program.cs
@@ -4,10 +4,10 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection") ?? 
-    $"Server=db;Database=ProductInventoryDb;User  Id=sa;Password={Environment.GetEnvironmentVariable("DB_PASSWORD")};"));
+    "Server=db;Database=ProductInventoryDb;User Id=sa;Password=Kriv@P@lank@5;"));
+
 
 
 builder.Services.AddEndpointsApiExplorer();
