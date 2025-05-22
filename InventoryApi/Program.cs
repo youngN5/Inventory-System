7e3cd72c3ec0276e3331417571fc144972d14e6e
Mladen Shkrbikj <skrbicc555@gmail.com>
2025-05-22 11:57:47 +0200

    Added Docker support for frontend and backend

--- InventoryApi/Program.cs
+++ InventoryApi/Program.cs
@@ -6,7 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection") ?? 
+    $"Server=db;Database=ProductInventoryDb;User  Id=sa;Password={Environment.GetEnvironmentVariable("DB_PASSWORD")};"));
+
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -35,6 +37,12 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 app.MapControllers();
 
